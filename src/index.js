9e5389c740980433e10efcffe63a5f9b0ae2c9d4
iranab04 <ranabag3132004@gmail.com>
2026-02-16 11:17:46 +0530

    Initial setup

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <h1>hello!</h1>
+      <h1>starting new project!</h1>
     </div>
   </React.StrictMode>
 );
